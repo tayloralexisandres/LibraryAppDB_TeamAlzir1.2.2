37d9a7785349bed0ce761ccd37c0986dc10fefae
mehmetCt <mehmet@cybertekschool.com>
2023-03-29 12:37:05 -0400

    LibraryApp --> US03 is Started

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -2,7 +2,7 @@
 -- US 01
 select count(id) from users; --
 -- 1855
-
+-- US 01-1
 select count(distinct id) from users;
 -- 1855
 
@@ -10,10 +10,14 @@ select count(distinct id) from users;
 -- RESULT --> MANUALLY IT IS PASSED
 
 
--- US 02
+-- US 01-2
 select * from users;
 
 
--- US 03
+-- US 02
 select count(*) from book_borrow
 where is_returned=0;
+
+-- US 03
+select name from book_categories;
+
